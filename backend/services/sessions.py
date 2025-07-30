609549d7792bda8de787cf2eeba1c8353f99403e
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-07-30 00:22:39 -0700

    Changes to db.py, starter code for storage.py + Comment on auth-slice.ts for preparation

--- backend/services/sessions.py
+++ backend/services/sessions.py
@@ -6,7 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Stores the user.id as soon as create_user() is called and the search history and user preferences for users that haven't logged in
-# When the user creates an account, the user row is returned with the user_id. store this returned user_id in redis. to access redis, look into the cookie and fetch the session id
+# When the user creates an account, the user row is returned with the user_id. store this returned user_id in redis. to access redis, look into the cookie and fetch the session id, then query redis to fetch the row that corresponds with that session_id, then fetch the user_id
+# if the user logs out, delete all session data of the corresponding session_id from redis
+# if user_id is not found in redis, the user is not logged in
+# every 60 days, destroy the cookie and its session_id row from redis to force the user to log out again
+# if the user isnt logged in and submits feedback, a developer response isn't needed and the user response sender will be anonymous
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=os.getenv("REDIS_PORT"),
---
+++ frontend/src/store/slices/auth-slice.ts
@@ -0,0 +1,2 @@
+// stores information such as if the user is logged in
+// constantly check with useeffect to check if the user is logged in via checking if user_id is in redis
