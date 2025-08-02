50fc7ede55b20ff6590f259c971e9991714395e9
brian-alan-huynh <brian.alan.huynh@gmail.com>
2025-08-02 01:55:10 -0700

    Add startup code for Confluent Kafka

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,10 @@
+import threading
+
+from infrastructure.messaging import run_consumer
+
 # initalize classes from infrastructure and services here
+
+@app.on_event("startup")
+def start_kafka_consumer():
+    thread = threading.Thread(target=run_consumer, daemon=True)
+    thread.start()
